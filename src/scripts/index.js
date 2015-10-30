ce70e42eeee0b782cbe126fb6efa9a5355cdf695
Milos Nedeljkovic <dr.kiman@gmail.com>
2015-10-30 23:12:50 +0100

    chnaged model to order

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,30 +14,61 @@ import ReactDOM from "react-dom";
 import t from "tcomb-form";
 var Form = t.form.Form;
 
-var Person = t.struct({
-    name: t.String,
-    surname: t.String,
-    age: t.Number,
-    tags: t.list(t.String)
+var Address = t.struct({
+    street: t.String,
+    number: t.String,
+    city: t.String,
+    zip: t.String,
+    country: t.String
+});
+
+var OrderItem = t.struct({
+    description: t.String,
+    price: t.Number,
+    quantity: t.Number
+})
+
+var Order = t.struct({
+    description: t.String,
+    activatedDate: t.Date,
+    billingAddress: Address,
+    items: t.list(OrderItem)
 });
 
 class App extends React.Component {
     constructor() {
         super();
+        this.state = {
+            orderFormValue: {
+                activatedDate: new Date()
+            }
+        }
     }
     save() {
         var value = this.refs.form.getValue();
         if (value)
             console.log(value);
-        else
-            console.log(this.refs.form.validate());
+        else {
+            var validationResult = this.refs.form.validate();
+            console.log(validationResult);
+        }
+    }
+    onChange(value, path, kind){
+        console.log({
+            value,
+            path,
+            kind
+        });
+        this.setState({orderFormValue:value});
     }
     render() {
         return (
             <div>
                 <Form
                     ref="form"
-                    type={Person}
+                    type={Order}
+                    value={this.state.orderFormValue}
+                    onChange={this.onChange.bind(this)}
                 />
                 <button onClick={this.save.bind(this)}>Save</button>
             </div>
